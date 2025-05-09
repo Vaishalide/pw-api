a2b66ef20f9d6013423e4aa115373a6f25a0938f
isha dalvi <dipeshxyzyadav@gmail.com>
2025-05-09 10:14:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 const data = require('./data.json'); // Load local JSON file
 
 app.use(cors()); // Enable CORS for all origins
+app.get('/', (req, res) => {
+  res.send('✅ PW API is running! ');
+});
 
 app.get('/data', (req, res) => {
   res.json(data);
