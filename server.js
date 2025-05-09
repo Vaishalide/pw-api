44584529fce08de056bd6978e5c165e83cf68320
rongamingid <dipeshy636@gmail.com>
2025-05-09 09:09:55 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const data = require('./data.json'); // Load local JSON file
+
+app.use(cors()); // Enable CORS for all origins
+
+app.get('/data', (req, res) => {
+  res.json(data);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
