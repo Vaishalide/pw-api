0a5c4189c40d14acd9ce27bad95347a89d47b123
isha dalvi <dipeshxyzyadav@gmail.com>
2025-05-09 13:44:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,12 +33,14 @@ app.get('/data/batches/:batchId/subjects/:subjectId/topics', (req, res) => {
   const topics = Object.entries(subject.topics).map(([key, val]) => ({
     key,
     name: val.name,
-    lectures: val.lectures || [],
-    notes: val.notes || [],
-    dpps: val.dpps || []
+    lectures: Array.isArray(val.lectures) ? val.lectures : Object.values(val.lectures || {}),
+    notes: Array.isArray(val.notes) ? val.notes : Object.values(val.notes || {}),
+    dpps: Array.isArray(val.dpps) ? val.dpps : Object.values(val.dpps || {})
   }));
+
   res.json(topics);
 });
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
