f57eabc8be599602fd092a2b3e11a4aa9f09da97
isha dalvi <dipeshxyzyadav@gmail.com>
2025-06-25 08:16:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const COOKIE_DATA = {
   domain: ".streamfiles.eu.org",
   cookies: {
-    verified_task: "dHJ1ZQ==",
+    verified_task: "d",
     countdown_end_time: "MTc1MDgyMjgzNzQ5Nw=="
   },
   target_url: "https://others.streamfiles.eu.org/cw",
