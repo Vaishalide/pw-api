7a8b59635608c264ca535956ec711bcd288c8605
isha dalvi <dipeshxyzyadav@gmail.com>
2025-08-26 20:44:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ if (cluster.isPrimary) {
   // CORS Configuration
   const allowedOrigins = [
     'https://pwthor.site',
-    'https://www.pwjarvis.com',
+    'https://testthor-01a6fc8e03ea.herokuapp.com',
     'http://localhost:3000',
     'http://127.0.0.1:5500'
   ];
