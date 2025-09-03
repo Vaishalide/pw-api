36cc25ff669bdcb1542d371b399c3ac4f5eb5bf5
isha dalvi <dipeshxyzyadav@gmail.com>
2025-09-03 17:39:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ if (cluster.isPrimary) {
     'https://pwthor.site',
     'https://testthor-01a6fc8e03ea.herokuapp.com',
     'http://localhost:3000',
-    'http://127.0.0.1:5500'
+    'https://studymeta.in'
   ];
   const corsOptions = {
     origin: (origin, callback) => {
